ee4a8b0f237fae44bac52b28c835376787e9bf66
Adson de Oliveira Coelho <adson.coelho@gmail.com>
2022-07-27 23:08:46 +0000

    Criação de tablea Post

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
 from flask import Flask, render_template
 from datetime import datetime
-
+from flask_sqlalchemy import SQLAlchemy
+ 
 app = Flask("hello")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-posts = [
-    {
-        "title": "O meu primeiro post",
-        "body": "Aqui é o texto do Post",
-        "author": "Feulo",
-        "created": datetime(2022,7,25)
-    },
-    {
-        "title": "O meu segundo post",
-        "body": "Aqui é o texto do Post",
-        "author": "Danilo",
-        "created": datetime(2022,7,26)
-    },
-]
+db = SQLAlchemy(app)
+
+class Post(db.model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    author = db.Collumn(db.String(20))
+    created = db.Collumn(db.Datetime, nullable=False, default=datetime.now)
+
 
 @app.route("/")
 def index():
+    # Busca no banco os posts
     return render_template("index.html", posts=posts)
-
-
-
-
-
-
