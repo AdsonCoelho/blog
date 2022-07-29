dd3e63bb5e51c38fec8ed6056116d85c30ec5e5f
Adson de Oliveira Coelho <adson.coelho@gmail.com>
2022-07-29 00:33:36 +0000

    Criação de loogin e logout

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
 from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.exc import IntegrityError
 
 app = Flask("hello")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = "pudin"
+app.config["SECRET_KEY"] = "pudim"
 
 db = SQLAlchemy(app)
-Login = LoginManager(app)
+login = LoginManager(app)
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -28,15 +29,16 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     posts = db.relationship('Post', backref='author')
 
-def set_password(self, password):
-    self.password_hash = generate_password_hash(passwaord)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-def check_password(self, password):
-    return check_password_hash(self.passwaord_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-@Login.user_loader
+@login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 db.create_all()
 
@@ -45,6 +47,42 @@ def index():
     posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
-@app.route('/register')
+@app.route('/register', methods=["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            new_user = User(username=username, email=email)
+            new_user.set_password(password)
+            db.session.add(new_user)
+            db.session.commit()
+        except IntegrityError:
+            flash("Username or E-mail already exists!")
+        else:
+            return redirect(url_for('login'))
     return render_template('register.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user is None or not user.check_password(password):
+            flash("Incorrect Username or Password")
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
